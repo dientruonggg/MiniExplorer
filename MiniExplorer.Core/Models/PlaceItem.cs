50fcc5c0f86215656da2d0ef71199fbdf8f5693b
dientruonggg <dientruong2104@gmail.com>
2026-02-04 13:19:41 +0700

    finish =)))

--- MiniExplorer.Core/Models/PlaceItem.cs
+++
@@ -1,8 +0,0 @@
-namespace MiniExplorer.Core.Models;
-
-public class PlaceItem
-{
-    public string Name { get; set; } = string.Empty;
-    public string Path { get; set; } = string.Empty;
-    public string Icon { get; set; } = string.Empty;
-}
